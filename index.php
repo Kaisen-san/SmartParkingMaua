05061346ae34d53b287f330b4744443cbdf4a27a
KaisenSan <fasa.work@gmail.com>
2017-11-02 13:58:11 -0200

    Implement DB connection

--- index.php
+++ index.php
@@ -10,6 +10,41 @@ $gate = $obj['gate'];
 
 #var_dump($obj);
 
-$sql = "SELECT * FROM tbParking WHERE gate = ".$gate;
+if ($action == "entrance") {
+    $action = 1;
+} elseif ($action == "exit") {
+    $action = 0;
+}
 
-#echo $sql;
+if ($gate == "main") {
+    $gate = 1;
+} elseif ($gate == "secondary") {
+    $gate = 2;
+}
+
+
+# DB credentials
+$servername = "localhost";
+$username = "spm";
+$password = "maua"
+
+# Create connection
+$conn = new mysqli($servername, $username, $password);
+
+# Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO tbParking (timestamp, action, gate)
+VALUES ($timestamp, $action, $gate)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn-error;
+}
+
+$conn->close();
+
+?>
